7e666d83623be4179e046e865446c7f17594cc89
taku10101 <taku1011ht@gmail.com>
2024-09-28 00:58:21 +0900

    add 404 path

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { Hono } from "hono";
 import { CreateBookRoute, GetBooksRoute } from "./open_api/route/book";
 import { swaggerUI } from "@hono/swagger-ui";
 import api from "./open_api/route";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono();
+app.use(prettyJSON());
+app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 
 app.get("/", (c) => {
   return c.json({ message: "Hello, World!" });
