c66fb9c6a3cb7187e2ed1613dbc7abc7cc80980d
taku10101 <taku1011ht@gmail.com>
2024-09-28 01:24:53 +0900

    add crud

--- src/open_api/route/book.ts
+++ src/open_api/route/book.ts
@@ -7,6 +7,8 @@ import {
 } from "../schema/book";
 import { postRoute } from "../util/post";
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { putRoute } from "../util/put";
+import { deleteRoute } from "../util/delete";
 
 export const GetBooksRoute = getRoute({
   path: "books",
@@ -25,9 +27,27 @@ export const CreateBookRoute = postRoute({
   responsesSchema: BookSchema,
 });
 
+export const UpdateBookRoute = putRoute({
+  path: "books/{id}",
+  paramsSchema: BookParams,
+  requestBodySchema: BookRequestBody,
+  responsesSchema: BookSchema,
+});
+
+export const DeleteBookRoute = deleteRoute({
+  path: "books/{id}",
+  paramsSchema: BookParams,
+  responsesSchema: BookSchema,
+});
 export const BookApi = new OpenAPIHono();
 
-const api = [GetBooksRoute, GetBookRoute, CreateBookRoute];
+const api = [
+  GetBooksRoute,
+  GetBookRoute,
+  CreateBookRoute,
+  UpdateBookRoute,
+  DeleteBookRoute,
+];
 
 api.forEach((route) => {
   BookApi.openapi(route, (c) => {
